93eb9d71ea9dd4083444302851b50edb89cf632d
Nika Nozadze <nikanozadze1010@gmail.com>
2023-07-31 13:27:07 +0400

    feat: reate board delte request

--- src/controllers/board-controller.ts
+++ src/controllers/board-controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { Board, Column } from "models";
+import { Board, Column, Subtask, Task } from "models";
 import { createBoardSchema } from "schemas";
 
 export const createBoard = async (req: Request, res: Response) => {
@@ -39,5 +39,40 @@ export const createBoard = async (req: Request, res: Response) => {
     return res.status(201).json(newBoard);
   } catch (error) {
     return res.status(401).json(error);
+  }
+};
+
+export const deleteBoard = async (req: Request, res: Response) => {
+  const boardId = req.params.id;
+
+  try {
+    const board = await Board.findOne({ id: boardId });
+
+    if (!board) {
+      return res.status(404).json({ message: "Board not found" });
+    }
+
+    const columns = await Column.find({ _id: { $in: board.columns } });
+
+    const tasks = await Task.find({
+      _id: { $in: columns.map((col) => col.tasks).flat() },
+    });
+
+    const subtasks = await Subtask.find({
+      _id: { $in: tasks.map((task) => task.subtasks).flat() },
+    });
+
+    await Promise.all([
+      Task.deleteMany({ _id: { $in: tasks.map((task) => task._id) } }),
+      Subtask.deleteMany({
+        _id: { $in: subtasks.map((subtask) => subtask._id) },
+      }),
+      Column.deleteMany({ _id: { $in: columns.map((col) => col._id) } }),
+      board.deleteOne(),
+    ]);
+
+    res.json({ message: "Board and associated data deleted successfully." });
+  } catch (error) {
+    res.status(500).json(error);
   }
 };
