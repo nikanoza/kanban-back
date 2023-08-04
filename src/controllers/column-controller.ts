3f0ce7f7d63f29ac9f4df00a2a1662449ea4cb30
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-04 11:40:37 +0400

    feat: create column post request function

--- src/controllers/column-controller.ts
+++ src/controllers/column-controller.ts
@@ -1,7 +1,37 @@
 import { Request, Response } from "express";
+import { Board, Column } from "models";
+import { createColumnSchema } from "schemas";
 
-export const createColumn = (req: Request, res: Response) => {
+export const createColumn = async (req: Request, res: Response) => {
   try {
     const { body } = req;
-  } catch (error) {}
+
+    const validator = await createColumnSchema(body);
+
+    const { value, error } = validator.validate(body);
+
+    if (error) {
+      return res.status(401).json(error.details);
+    }
+
+    const { title, boardId } = value;
+
+    const board = await Board.findOne({ id: boardId });
+    if (!board) {
+      return res.status(404).json({ message: "Board not found" });
+    }
+
+    const column = new Column({
+      title,
+      tasks: [],
+    });
+    await column.save();
+
+    board.columns.push(column._id);
+    await board.save();
+
+    return res.status(201).json(board);
+  } catch (error) {
+    return res.status(401).json(error);
+  }
 };
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,2 +1,3 @@
 export { default as createBoardSchema } from "./add-board-schema";
 export { default as updateBoardSchema } from "./update-board-schema";
+export { default as createColumnSchema } from "./add-column-schema";
