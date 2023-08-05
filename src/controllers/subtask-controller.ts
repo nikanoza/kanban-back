6e9c13f63fe9fabbc873daa37cbb3bf7c4f1947f
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-05 16:13:16 +0400

    feat: create subtask put request function

--- src/controllers/subtask-controller.ts
+++ src/controllers/subtask-controller.ts
@@ -9,7 +9,7 @@ export const createSubtask = async (req: Request, res: Response) => {
     if (!task) {
       return res.status(404).json({ message: "task not found" });
     }
-    if (typeof title === "string" && title.length > 0) {
+    if (typeof title === "string" && title.trim().length > 0) {
       const newSubtask = new Subtask({
         title,
         active: false,
@@ -45,6 +45,32 @@ export const deleteSubtask = async (req: Request, res: Response) => {
     return res.json({
       message: "subtask deleted successfully.",
     });
+  } catch (error) {
+    return res.status(401).json(error);
+  }
+};
+
+export const updateSubtask = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.subtaskId;
+    const { title, active } = req.body;
+
+    if (
+      typeof title === "string" &&
+      title.trim().length > 0 &&
+      typeof active === "boolean"
+    ) {
+      const subtask = await Subtask.findOne({ id });
+      if (!subtask) {
+        return res.status(404).json({ message: "subtask not found" });
+      }
+
+      subtask.title = title;
+      subtask.active = active;
+      await subtask.save();
+    } else {
+      return res.status(401).json({ message: "invalid data" });
+    }
   } catch (error) {
     return res.status(401).json(error);
   }
