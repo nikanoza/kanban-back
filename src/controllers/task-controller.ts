48e2d5b001e326b2011b6d4ac6b69c52920bd345
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-05 05:24:14 +0400

    feat: create task post request function

---
+++ src/controllers/task-controller.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import { Subtask, Task } from "models";
+import { addTaskSchema } from "schemas";
+
+export const createBoard = async (req: Request, res: Response) => {
+  try {
+    const { body } = req;
+
+    const validator = await addTaskSchema(body);
+    const { value, error } = validator.validate(body);
+
+    if (error) {
+      return res.status(401).json(error.details);
+    }
+
+    const { title, description, subtasks } = value;
+
+    const newTask = new Task({
+      title,
+      description,
+    });
+
+    const subtasksArray = [];
+
+    for (let i = 0; i < subtasks.length; i++) {
+      const newSubtask = new Subtask({
+        title: subtasks[i],
+        active: false,
+      });
+
+      await newSubtask.save();
+      subtasksArray.push(newSubtask._id);
+    }
+
+    newTask.subtasks = subtasksArray;
+
+    newTask.save();
+    return res.status(201).json(newTask);
+  } catch (error) {
+    return res.status(401).json(error);
+  }
+};
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,3 +2,4 @@ export { default as createBoardSchema } from "./add-board-schema";
 export { default as updateBoardSchema } from "./update-board-schema";
 export { default as createColumnSchema } from "./add-column-schema";
 export { default as updateColumnSchema } from "./update-column-schema";
+export { default as addTaskSchema } from "./add-task-schema";
